b5469fd6bf62a4b69d9aa5086054352529ee176f
Leo Tejeda <leonelhtejeda@gmail.com>
2018-10-22 18:39:36 -0700

    first commit

---
+++ fundamentals.js
@@ -0,0 +1,24 @@
+//5 primitive Datatypes
+//4
+//9.3
+//-10
+
+//Strings
+//"Hello World"
+//"43"
+
+//Booleans
+//true
+//false
+
+//null and undefined
+//null
+//Undefined
+
+let string = ["hello world", "second index"];
+let newString = []
+
+for(let i = 0; i < string.length; i++){
+   newString.push(string[i][1])
+}
+console.log(newString)
