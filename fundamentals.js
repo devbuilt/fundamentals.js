123524fad57d3495fe1a03310cdce22ff6ea9ce2
Leo Tejeda <leonelhtejeda@gmail.com>
2018-10-25 20:33:03 -0700

    prompt

--- fundamentals.js
+++ fundamentals.js
@@ -18,7 +18,44 @@
 let string = ["hello world", "second index"];
 let newString = []
 
+
 for(let i = 0; i < string.length; i++){
-   newString.push(string[i][1])
+   newString.push(string[i])
 }
-console.log(newString)// 'e' 'e'
+newString.push('You are cool', 5,78)
+
+console.log(newString)
+console.log(string)
+
+
+//variable is a container that has a name on it.
+//They follow this patterns
+let yourValue = "hello";
+let yourVariableName = yourValue;
+console.log(yourVariableName)
+// They can store all of the values we've seen
+let name = "Rusty";
+let secretNumber = 73;
+let isAdorable = true;
+
+//Recal the stored values by calling the variable name
+
+let namess = "Rusty";
+"Hello there " + namess //"Hello there Rusty"
+
+let num = 37;
+num + 3 + 10 //50
+
+//we can also update exsiting variables
+let names = "Robert";
+names = "Bob";
+console.log(names)
+
+
+
+let firstName = prompt("What is your first Name? ");
+let lastName = prompt("What is your last name? ");
+let userAge = prompt("How old are you? ");
+let fullName = firstName + " " + lastName;
+alert("Hello " + fullName);
+alert("You are " + userAge + " years old.");
